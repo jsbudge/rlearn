Added MUSIC back into the direction finding. Added preprocessing layer to get normalization to work better. Also fixed up Colton's code to do better memory management.

rlearn/DFJeff.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from mpl_toolkits import mplot3d
 from numba import cuda
+import cupy as cupy
 import numba
 from cmath import exp
 
@@ -198,12 +199,15 @@ def _runMUSICGPU(a_azimuthGrid, a_elevationGrid, a_spectrumMUSIC,
         space) of the signal.
     """
     # Precalculations for GPU processing
-    nullMatrix = a_nullSpace @ np.conjugate(a_nullSpace.T)
-    azimuthSin = np.sin(a_azimuthGrid)
-    azimuthCos = np.cos(a_azimuthGrid)
-    elevationSin = np.sin(a_elevationGrid)
-    elevationCos = np.cos(a_elevationGrid)
+    d_nullMatrix = cupy.array(a_nullSpace @ np.conjugate(a_nullSpace.T), dtype=np.complex128)
+    d_azimuthSin = cupy.array(np.sin(a_azimuthGrid), dtype=np.float64)
+    d_azimuthCos = cupy.array(np.cos(a_azimuthGrid), dtype=np.float64)
+    d_elevationSin = cupy.array(np.sin(a_elevationGrid), dtype=np.float64)
+    d_elevationCos = cupy.array(np.cos(a_elevationGrid), dtype=np.float64)
     spectrumSize = np.size(a_azimuthGrid) * np.size(a_elevationGrid)
+
+    d_relativePositionBody = cupy.array(a_relativePositionBody, dtype=np.float64)
+    d_spectrumMUSIC = cupy.array(a_spectrumMUSIC, dtype=np.float64)
 
     # Use Numba to interface with GPU
     # Calculate the number of threads per block
@@ -221,25 +225,36 @@ def _runMUSICGPU(a_azimuthGrid, a_elevationGrid, a_spectrumMUSIC,
 
     # Call the CUDA Kernal
     _musicSearchGPU[blocksPerGrid, threadsPerBlock](
-        nullMatrix, a_waveLength, a_antennaNumber,
-        a_relativePositionBody, azimuthSin, azimuthCos,
-        elevationSin, elevationCos, a_spectrumMUSIC)
+        d_nullMatrix, a_waveLength, a_antennaNumber,
+        d_relativePositionBody, d_azimuthSin, d_azimuthCos,
+        d_elevationSin, d_elevationCos, d_spectrumMUSIC)
     cuda.synchronize()
 
     # Peak Estimation on the GPU
-    isPeak = np.zeros(np.shape(a_spectrumMUSIC), dtype=int)
+    d_isPeak = cupy.array(np.zeros(np.shape(a_spectrumMUSIC), dtype=int), dtype=int)
     _estimatePeaksGPU[blocksPerGrid, threadsPerBlock](
-        isPeak, a_spectrumMUSIC, a_targetNumber)
+        d_isPeak, d_spectrumMUSIC, a_targetNumber)
     cuda.synchronize()
 
+    spectrumMUSIC = d_spectrumMUSIC.get()
+    isPeak = d_isPeak.get()
+
+    del d_isPeak
+    del d_spectrumMUSIC
+    del d_azimuthSin
+    del d_nullMatrix
+    del d_elevationSin
+    del d_azimuthCos
+    del d_elevationCos
+    del d_relativePositionBody
+
     # Extract peak indices
-    peakIndices = _extractPeaks(isPeak, a_spectrumMUSIC,
+    peakIndices = _extractPeaks(isPeak, spectrumMUSIC,
                                 a_targetNumber)
     estimatedAngles = np.array(
-        [a_azimuthGrid[peakIndices[:, 0]], \
-         a_elevationGrid[peakIndices[:, 1]]]).T
+        [a_azimuthGrid[peakIndices[:, 0]], a_elevationGrid[peakIndices[:, 1]]]).T
 
-    return a_spectrumMUSIC, peakIndices, estimatedAngles
+    return spectrumMUSIC, peakIndices, estimatedAngles
 
 
 @cuda.jit

rlearn/wave_env.py
@@ -27,7 +27,7 @@ from celluloid import Camera
 
 def db(x):
     ret = abs(x)
-    ret[ret == 0] = 1e-9
+    ret[ret < 1e-15] = 1e-15
     return 20 * np.log10(ret)
 
 
@@ -105,7 +105,7 @@ class SinglePulseBackground(Environment):
 
         # Antenna array definition
         self.tx_locs = np.array([(.5, 0, 0), (-.5, 0, 0)]).T
-        self.rx_locs = np.array([(.25, .5, 0), (0, 0, 0)]).T
+        self.rx_locs = np.array([(0, .5, 0), (0, 0, 0)]).T
         self.el_rot = lambda el, loc: np.array([[1, 0, 0],
                                                 [0, np.cos(el), -np.sin(el)],
                                                 [0, np.sin(el), np.cos(el)]]).dot(loc)
@@ -149,7 +149,7 @@ class SinglePulseBackground(Environment):
         return self.max_steps
 
     def states(self):
-        return dict(cpi=dict(type='float', shape=(self.nsam, self.cpi_len)),
+        return dict(cpi=dict(type='float', shape=(self.nsam, self.cpi_len), min_value=-300, max_value=100),
                     currscan=dict(type='float', shape=(1,), min_value=self.az_lims[0], max_value=self.az_lims[1]),
                     currelscan=dict(type='float', shape=(1,), min_value=self.el_lims[0], max_value=self.el_lims[1]),
                     currwave=dict(type='float', shape=(WAVEPOINTS, self.n_tx), min_value=0, max_value=1),
@@ -191,10 +191,7 @@ class SinglePulseBackground(Environment):
         for idx, ap in enumerate(self.apc):
             rda = genRDAMap(cpi[:, :, ap[0]], fft_chirp[:, ap[1]], self.nsam)
             st = db(rda)
-            if np.std(st) > 0:
-                st = (st - np.mean(st)) / np.std(st)
-            else:
-                pass
+            st[st > 100] = 100
             state[:, :, idx] = st
             curr_cpi[:, :, idx] = genRD(cpi[:, :, ap[0]], fft_chirp[:, ap[1]], self.nsam)
         self.curr_cpi = np.fft.fft(curr_cpi, self.fft_len, axis=0)[self.fft_len // 2 - 1:, :, :]
@@ -230,6 +227,7 @@ class SinglePulseBackground(Environment):
         '''
 
         # Target detection
+        '''
         t_poss = [[] for n in range(self.v_ants)]
         f_poss = []
         for va_rx in range(self.v_ants):
@@ -290,30 +288,33 @@ class SinglePulseBackground(Environment):
             ea = []
             el_sc += motion
             az_sc += motion
+        '''
 
         # Colton MUSIC input here
-        '''
-        if len(f_poss) > 0:
-            det_sc += 1
-            single_pulse = np.fft.ifft(cpi[:, 0, :], axis=0)[:self.nsam, :].T
-            ea = music(single_pulse, 1, va.T, self.fc)
-            # ea = music(single_pulse, 1, va.T, self.fc, .5 * DTR,
-            #            np.array([self.az_pt - self.az_bw * 2, self.az_pt + self.az_bw * 2]),
-            #            np.array([self.el_pt - self.el_bw * 2, self.el_pt + self.el_bw * 2]),
-            #            False, False)
-            if ea.shape[1] > 0:
-                eldiff = cpudiff(ea[1][0], self.el_pt)
-                azdiff = cpudiff(ea[0][0], self.az_pt)
+        single_pulse = np.fft.ifft(cpi[:, 0, :], axis=0)[:self.nsam, :].T
+        ea = np.zeros((2,))
+        n_hits = 0
+        for n in range(self.n_tx):
+            va_1 = va[:, [True if a[1] == n else False for a in self.apc]].astype(np.float64)
+            mus_res = music(single_pulse, 1, va_1.T, self.fc[n], .01 * DTR,
+                            a_scaleElevation=False).flatten()
+            ea += 0 if len(mus_res) == 0 else mus_res
+            n_hits += 0 if len(mus_res) == 0 else 1
 
-                # Only add reward if music finds possible angles
-                el_sc += 1 - abs(eldiff / self.el_bw)
-                az_sc += 1 - abs(azdiff / self.az_bw)
+        if n_hits > 0:
+            det_sc += 1
+            ea /= n_hits
+            eldiff = cpudiff(ea[1], self.el_pt)
+            azdiff = cpudiff(ea[0], self.az_pt)
+
+            # Only add reward if music finds possible angles
+            el_sc += 1 - abs(eldiff / self.el_bw)
+            az_sc += 1 - abs(azdiff / self.az_bw)
         else:
             # No targets found, incentivize the antenna to move
             ea = []
             el_sc += motion
             az_sc += motion
-        '''
 
         # Add all the disparate reward functions
         reward += amb_sc + res_sc + det_sc + el_sc + az_sc
@@ -344,10 +345,10 @@ class SinglePulseBackground(Environment):
                 'currbw': [100e6 for n in range(self.n_tx)]}
 
     def genCPI(self, chirp, tf, az_pt, el_pt):
-        mx_threads = cuda.get_current_device().MAX_THREADS_PER_BLOCK // 2
+        mx_threads = cuda.get_current_device().MAX_THREADS_PER_BLOCK // 4
         cpi_threads = int(np.ceil(self.cpi_len / self.samples))
         samp_threads = mx_threads // cpi_threads - 1
-        threads_per_block = (2, 16)  # (cpi_threads, samp_threads)
+        threads_per_block = (16, 16)  # (cpi_threads, samp_threads)
         blocks_per_grid = (
             int(np.ceil(self.cpi_len / cpi_threads)), int(np.ceil(self.samples / samp_threads)))
         sub_blocks = (int(np.ceil(self.cpi_len / threads_per_block[0])),
@@ -367,7 +368,7 @@ class SinglePulseBackground(Environment):
         bg_gpu = cupy.fft.ifft2(bg_gpu, axes=(1, 2))
         gpts_gpu = cupy.array(gpts, dtype=np.float64)
         for sub in self.env.targets:
-            sv.append([sub(t) for t in tf])
+            sv.append([sub(t, fullset=True) for t in tf])
         sub_pos = cupy.array(np.ascontiguousarray(sv), dtype=np.float64)
         alocs_rx = self.el_rot(el_pt, self.az_rot(az_pt, self.rx_locs))
         alocs_tx = self.el_rot(el_pt, self.az_rot(az_pt, self.tx_locs))
@@ -587,8 +588,13 @@ class Sub(object):
         # Plot out next f_ts of movement for reproducability
         self.plotRoute(f_ts, min_x, max_x, min_y, max_y)
 
-    def __call__(self, t):
-        return self.surf(t), *self.pos(t)
+    def __call__(self, t, fullset=False):
+        if fullset:
+            dd = self.vel(t) - self.vel(t+.01)
+            dang = [dd[0] / np.linalg.norm(dd), dd[1] / np.linalg.norm(dd)]
+            return self.surf(t), *self.pos(t), *dang
+        else:
+            return self.surf(t), *self.pos(t)
 
     def reset(self):
         # Plot out next f_ts of movement for reproducability
@@ -619,13 +625,13 @@ class Sub(object):
                 vels[1] = -vels[1]
             loc += vels * t0
             pos[:, idx] = loc + 0.0
-            is_surfaced[idx] = surfaced * 50
+            is_surfaced[idx] = surfaced * 9.5
         pn = UnivariateSpline(np.linspace(0, f_ts, f_ts * 100), pos[0, :])
         pe = UnivariateSpline(np.linspace(0, f_ts, f_ts * 100), pos[1, :])
         surf = interp1d(np.linspace(0, f_ts, f_ts * 100), is_surfaced, fill_value=0)
         self.pos = lambda t: np.array([pe(t), pn(t)])
-        vn = np.gradient(pos[0, :]) / 100
-        ve = np.gradient(pos[1, :]) / 100
+        vn = UnivariateSpline(np.linspace(0, f_ts, f_ts * 100), np.gradient(pos[0, :]) / 100)
+        ve = UnivariateSpline(np.linspace(0, f_ts, f_ts * 100), np.gradient(pos[1, :]) / 100)
         self.vel = lambda t: np.array([ve(t), vn(t)])
         self.surf = surf
 
@@ -762,39 +768,48 @@ def genSubProfile(pathrx, pathtx, subs, pan, el, pd_r, pd_i, params):
 
         sub_x = subs[subnum, tt, 1]
         sub_y = subs[subnum, tt, 2]
-        spow = subs[subnum, tt, 0]
-        sub_z = 20 if spow > 0 else 0
+        sub_cos = subs[subnum, tt, 3]
+        sub_sin = subs[subnum, tt, 4]
+        spow = subs[subnum, tt, 0] * 20 / 7.6
+        sub_z = subs[subnum, tt, 0]
 
         # Get LOS vector in XYZ and spherical coordinates at pulse time
-        for n in range(-3, 3):
-            s_tx = sub_x - pathtx[0, tt]
-            s_ty = sub_y - pathtx[1, tt]
-            s_tz = sub_z - pathtx[2, tt]
-            rngtx = math.sqrt(s_tx * s_tx + s_ty * s_ty + s_tz * s_tz) + c0 / params[4] * n
-            s_rx = sub_x - pathrx[0, tt]
-            s_ry = sub_y - pathrx[1, tt]
-            s_rz = sub_z - pathrx[2, tt]
-            rngrx = math.sqrt(s_rx * s_rx + s_ry * s_ry + s_rz * s_rz) + c0 / params[4] * n
-            rng = (rngtx + rngrx)
-            rng_bin = (rng / c0 - 2 * params[3]) * params[4]
-            but = int(rng_bin) if rng_bin - int(rng_bin) < .5 else int(rng_bin) + 1
-            if n_samples > but > 0:
-                el_tx = math.asin(-s_tz / rngtx)
-                az_tx = math.atan2(s_tx, s_ty)
-                eldiff = diff(el_tx, el[tt])
-                azdiff = diff(az_tx, pan[tt])
-                tx_elpat = abs(math.sin(params[0] * eldiff) / (params[0] * eldiff)) if eldiff != 0 else 1
-                tx_azpat = abs(math.sin(params[1] * azdiff) / (params[1] * azdiff)) if azdiff != 0 else 1
-                el_rx = math.asin(-s_rz / rngrx)
-                az_rx = math.atan2(s_rx, s_ry)
-                eldiff = diff(el_rx, el[tt])
-                azdiff = diff(az_rx, pan[tt])
-                rx_elpat = abs(math.sin(params[0] * eldiff) / (params[0] * eldiff)) if eldiff != 0 else 1
-                rx_azpat = abs(math.sin(params[1] * azdiff) / (params[1] * azdiff)) if azdiff != 0 else 1
-                att = tx_elpat * tx_azpat * rx_elpat * rx_azpat
-                acc_val = spow * att * cmath.exp(-1j * wavenumber * rng) * 1 / (rng * rng)
-                cuda.atomic.add(pd_r, (but, np.uint64(tt)), acc_val.real)
-                cuda.atomic.add(pd_i, (but, np.uint64(tt)), acc_val.imag)
+        xpts = 11
+        ypts = 7
+        for n in range(xpts):
+            for m in range(ypts):
+                shift_x = sub_x + (n - xpts // 2) / (xpts // 2) * 40.52 * sub_sin
+                shift_y = sub_y + 4.75 / 40.52 * math.sqrt(
+                    40.52 * 40.52 - ((n - xpts // 2) / (xpts // 2) * 40.52) *
+                    ((n - xpts // 2) / (xpts // 2) * 40.52)) * sub_cos * (m - ypts // 2) / (ypts // 2)
+                s_tx = shift_x - pathtx[0, tt]
+                s_ty = shift_y - pathtx[1, tt]
+                s_tz = sub_z - pathtx[2, tt]
+                rngtx = math.sqrt(s_tx * s_tx + s_ty * s_ty + s_tz * s_tz) + c0 / params[4]
+                s_rx = shift_x - pathrx[0, tt]
+                s_ry = shift_y - pathrx[1, tt]
+                s_rz = sub_z - pathrx[2, tt]
+                rngrx = math.sqrt(s_rx * s_rx + s_ry * s_ry + s_rz * s_rz) + c0 / params[4]
+                rng = (rngtx + rngrx)
+                rng_bin = (rng / c0 - 2 * params[3]) * params[4]
+                but = int(rng_bin) if rng_bin - int(rng_bin) < .5 else int(rng_bin) + 1
+                if n_samples > but > 0:
+                    el_tx = math.asin(-s_tz / rngtx)
+                    az_tx = math.atan2(s_tx, s_ty)
+                    eldiff = diff(el_tx, el[tt])
+                    azdiff = diff(az_tx, pan[tt])
+                    tx_elpat = abs(math.sin(params[0] * eldiff) / (params[0] * eldiff)) if eldiff != 0 else 1
+                    tx_azpat = abs(math.sin(params[1] * azdiff) / (params[1] * azdiff)) if azdiff != 0 else 1
+                    el_rx = math.asin(-s_rz / rngrx)
+                    az_rx = math.atan2(s_rx, s_ry)
+                    eldiff = diff(el_rx, el[tt])
+                    azdiff = diff(az_rx, pan[tt])
+                    rx_elpat = abs(math.sin(params[0] * eldiff) / (params[0] * eldiff)) if eldiff != 0 else 1
+                    rx_azpat = abs(math.sin(params[1] * azdiff) / (params[1] * azdiff)) if azdiff != 0 else 1
+                    att = tx_elpat * tx_azpat * rx_elpat * rx_azpat
+                    acc_val = spow * att * cmath.exp(-1j * wavenumber * rng) * 1 / (rng * rng)
+                    cuda.atomic.add(pd_r, (but, np.uint64(tt)), acc_val.real)
+                    cuda.atomic.add(pd_i, (but, np.uint64(tt)), acc_val.imag)
 
 
 def ellipse(x, y, a, b, ang):

rlearn/wave_training.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.signal.windows import taylor
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from matplotlib.cm import ScalarMappable
 import matplotlib.gridspec as gridspec
 from celluloid import Camera
 from tftb.processing import WignerVilleDistribution
@@ -24,7 +25,7 @@ def findPowerOf2(x):
     return int(2 ** (np.ceil(np.log2(x))))
 
 
-games = 500
+games = 5
 eval_games = 1
 max_timesteps = 128
 batch_sz = 64
@@ -32,8 +33,12 @@ batch_sz = 64
 # Pre-defined or custom environment
 env = SinglePulseBackground(max_timesteps)
 
-# Instantiate a Tensorforce agent
-wave_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shape=(env.nsam, env.cpi_len)),
+# Define preprocessing layer (just a normalization)
+state_prelayer = [dict(type='exponential_normalization', decay=.01)]
+
+# Instantiate wave agent
+wave_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shape=(env.nsam, env.cpi_len),
+                                                            min_value=-300.1, max_value=100),
                                                    currwave=dict(type='float', shape=(100, env.n_tx), min_value=0,
                                                                  max_value=1),
                                                    currfc=dict(type='float', shape=(env.n_tx,), min_value=8e9,
@@ -44,10 +49,14 @@ wave_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shape=
                                        fc=dict(type='float', shape=(env.n_tx,), min_value=8e9, max_value=12e9),
                                        bw=dict(type='float', shape=(env.n_tx,), min_value=10e6,
                                                max_value=env.fs / 2 - 5e6)
-                                       ), max_episode_timesteps=max_timesteps, batch_size=batch_sz, discount=.9,
+                                       ), state_preprocessing=state_prelayer,
+                          max_episode_timesteps=max_timesteps, batch_size=batch_sz, discount=.9,
                           learning_rate=5e-4,
-                          memory=max_timesteps, exploration=.1, entropy_regularization=.1)
-motion_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shape=(env.nsam, env.cpi_len)),
+                          memory=max_timesteps, exploration=.1, entropy_regularization=.1, variable_noise=.4)
+
+# Instantiate motion agent
+motion_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shape=(env.nsam, env.cpi_len),
+                                                              min_value=-300.1, max_value=100),
                                                      currscan=dict(type='float', shape=(1,), min_value=env.az_lims[0],
                                                                    max_value=env.az_lims[1]),
                                                      currelscan=dict(type='float', shape=(1,), min_value=env.el_lims[0],
@@ -57,9 +66,10 @@ motion_agent = Agent.create(agent='a2c', states=dict(cpi=dict(type='float', shap
                                                    max_value=env.az_lims[1]),
                                          elscan=dict(type='float', shape=(1,), min_value=env.el_lims[0],
                                                      max_value=env.el_lims[1])),
+                            state_preprocessing=state_prelayer,
                             max_episode_timesteps=max_timesteps, batch_size=batch_sz, discount=.9, learning_rate=5e-5,
                             memory=max_timesteps, exploration=.5,
-                            entropy_regularization=.1)
+                            entropy_regularization=.1, variable_noise=.4)
 
 # Training regimen
 reward_track = np.zeros(games)
@@ -136,8 +146,11 @@ for ant in range(env.n_tx):
                      env.nr, env.nr / env.fs, env.fc[ant], env.bw[ant])
     fftpulse = np.fft.fft(pulse, findPowerOf2(nr) * 1)
     rc_pulse = db(np.fft.ifft(fftpulse * (fftpulse * taylor(findPowerOf2(nr))).conj().T, findPowerOf2(nr) * 8))
-    plt.plot(np.fft.fftshift(rc_pulse))
+    plt.plot(np.arange(-len(rc_pulse) // 2, len(rc_pulse) // 2)[1:], np.fft.fftshift(rc_pulse)[1:])
     back_noise[:nr] += pulse
+plt.ylabel('dB')
+plt.xlabel('Lag')
+plt.legend(['Tx_{}'.format(n + 1) for n in range(env.n_tx)])
 
 try:
     wd = WignerVilleDistribution(back_noise)
@@ -269,3 +282,35 @@ for n in range(env.n_rx):
                env.virtual_array[1, n * env.n_tx:(n + 1) * env.n_tx],
                env.virtual_array[2, n * env.n_tx:(n + 1) * env.n_tx])
     ax.scatter(env.rx_locs[0, n], env.rx_locs[1, n], env.rx_locs[2, n], marker='*')
+
+'''
+fig = plt.figure('Ocean3D')
+xx, yy = np.meshgrid(np.linspace(0, env.env.eswath, 1500), np.linspace(0, env.env.swath, 1500))
+bgpts = np.array([xx.flatten(), yy.flatten()])
+gv, gz = env.env.getBG(bgpts.T, l[2][0])
+disp_oc = fftconvolve(gz.reshape(xx.shape), np.ones((30, 30)) / (30**2), mode='same')
+ax3d = fig.add_subplot(1, 2, 1, projection='3d')
+ax = fig.add_subplot(1, 2, 2)
+ax3d.plot_surface(xx, yy, disp_oc, rstride=10, cstride=10, cmap='ocean')
+ax3d.set_zlim([-1, 10])
+im2d = ax.imshow(disp_oc, extent=[0, env.env.swath, 0, env.env.eswath], cmap='ocean')
+ax.set_ylabel('Northing (m)')
+ax.set_xlabel('Easting (m)')
+plt.colorbar(ScalarMappable(cmap='ocean'), ax=ax, fraction=.046, pad=.04)
+pos = []
+amp = []
+pcols = []
+for t in l[2]:
+    spow, loc1, loc2 = s(t)
+    pos.append([loc1, loc2])
+    amp.append(spow + 1)
+    pcols.append(cols[idx])
+pos = np.array(pos)
+if len(pos) > 0:
+    ax.scatter(pos[:, 0], pos[:, 1], s=amp, c=pcols)
+    plt_rng = 2 * (np.linalg.norm(env.env.pos(l[2])[:, -1] - np.array([pos[-1, 0], pos[-1, 1], 1])) -
+                   np.linalg.norm(env.env.pos(l[2])[:, 0] - np.array([pos[0, 0], pos[0, 1], 1]))) / \
+              (l[2][-1] - l[2][0])
+    ax.text(pos[0, 0], pos[0, 1], f'{-plt_rng:.2f}', c='white')
+plt.tight_layout()
+'''